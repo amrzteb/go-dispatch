d01c9b1db524bed147bb3a7dd2674560665b2af8
Song Xiang <songxiang@corp-ci.com>
2020-01-22 13:39:01 +0800

    getLoad

--- dao/Worker.go
+++ dao/Worker.go
@@ -1,13 +1,14 @@
 package dao
 
 type Worker struct {
-	ID         int `json:"id" orm:"column(id);auto"`
-	appkey     string
-	channel    string
-	host       string
-	siteId     int
-	capacity   float64
-	active     int
-	baseWeight float64
-	weight     float64
+	ID          int `json:"id" orm:"column(id);auto"`
+	Appkey      string
+	Channel     string
+	Host        string
+	SiteId      int
+	Capacity    float64 `gorm:"default:1.5"`
+	CurrentLoad float64 `gorm:"default:0"`
+	Active      int `gorm:"default:1"`
+	BaseWeight  float64  `gorm:"default:1.5"`
+	Weight      float64	`gorm:"default:1.5"`
 }
--- service/worker.go
+++ service/worker.go
@@ -1,5 +1,44 @@
 package service
 
-func getLoadByFrequency(frequency string) float64 {
+import (
+	"awesomeProject6/common"
+	"awesomeProject6/dao"
+	"fmt"
+	"strconv"
+	"time"
+)
 
+func getLoadByTask(task dao.Task, frequency string) float64 {
+	if task.Type == dao.SingleTask {
+		return 0.3
+	}else if task.Type == dao.PeriodicTask {
+		return getLoadByFrequency(frequency)
+	}else {
+		panic("任务类型不存在")
+	}
+}
+
+
+func getLoadByFrequency(frequency string) float64 {
+	ns, err := time.ParseDuration(frequency)
+	if err != nil {
+		panic(err)
+	}
+	secondsStr := fmt.Sprintf("%0.f\n", ns.Seconds(), ns)
+	secondsInt, err := strconv.Atoi(secondsStr)
+	if err != nil {
+		panic("Frequency 参数不合法")
+	}
+	load := fmt.Sprintf("%.8f", 1 / secondsInt)
+	value, err := strconv.ParseFloat(load, 64)
+	if err != nil{
+		panic(err)
+	}
+	return value
+}
+
+func getLowLoadWorker(site dao.Site)  {
+
+	worker := dao.Worker{}
+	common.GormPool.First(&worker)
 }
